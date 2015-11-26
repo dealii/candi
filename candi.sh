7542c903444b97385d62d90f794b6aa2461b6e0c
Uwe Köcher <koecher@hsu-hamburg.de>
2015-11-26 22:23:11 +0100

    candi: ubuntu 16 xenial (development) detection

--- candi.sh
+++ candi.sh
@@ -535,7 +535,7 @@ guess_platform() {
             *:*:*Ubuntu*\ 12*)     echo ubuntu12;;
             *:*:*Ubuntu*\ 14*)     echo ubuntu14;;
             *:*:*Ubuntu*\ 15*)     echo ubuntu15;;
-            *:*:*Ubuntu*\ 16*)     echo ubuntu16;;
+            *:xenial*:*Ubuntu*)    echo ubuntu16;;
             *:Tikanga*:*)          echo rhel5;;
             *:Santiago*:*)         echo rhel6;;
             Scientific:Carbon*:*)  echo rhel6;;
